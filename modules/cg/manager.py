d996e48f44c93129b69e166e6f152b22cc4a1ae5
Vladimir <htcvlado@gmail.com>
2022-06-17 16:02:37 +0200

    Added CoinGeckoAPI fundamentals

--- modules/cg/manager.py
+++ modules/cg/manager.py
@@ -1,1 +1,24 @@
 from pycoingecko import CoinGeckoAPI
+cg = CoinGeckoAPI()
+
+# btc_price = cg.get_price(ids='bitcoin', vs_currencies='usd')
+
+
+def get_price(coin_id: str, currency: str = "usd") -> int:
+    r = cg.get_price(ids=coin_id, vs_currencies=currency)
+    return int(r[coin_id][currency])
+
+# Multiple coins/currencies in one request
+# cg.get_price(ids=["bitcoin", "ethereum"], vs_currencies=["usd", "eur"])
+
+# Price of single coin
+# price = get_price(
+#     coin_id='bitcoin',
+#     currency='usd'
+# )
+
+# coins_detailed = cg.get_coins() # Get detailed list of coins
+#
+# # coins = cg.get_coins_list() # Get basic list of coins
+# for coin in coins_detailed:
+#     print(coin["name"])
