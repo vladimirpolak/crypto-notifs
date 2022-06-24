91450fa26035ad042facae78e19c93b8320f572d
Vladimir <htcvlado@gmail.com>
2022-06-24 23:07:48 +0200

    Updated code structure in main

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 # get comments/users from post and save to db
+#   request first 12 posts
+#   request all comments of 1st post
+#   for every comment
+#       add user if is new
+#       verify comment
+#       add comment to the user if is new
+
 # get prices of the requested coins in db + save prices to db
+#   get all requested prices from database
+#   request price for all coins
+#   add prices to database
+
 # check if any desired condition is met
+#   get all price requests from db
+#   check if any condition is met
+
 # if condition is met, notify the particular user through dm
+#   get comment's user
+#   get user's pk
+#   get existing thread for user or create new dm
