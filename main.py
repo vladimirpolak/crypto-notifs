bf9a4ade260b079967a9ecf29a6f4440ec192b2b
Vladimir <htcvlado@gmail.com>
2022-06-24 02:55:43 +0200

    Added basic code bullet points

---
+++ main.py
@@ -0,0 +1,4 @@
+# get comments/users from post and save to db
+# get prices of the requested coins in db + save prices to db
+# check if any desired condition is met
+# if condition is met, notify the particular user through dm
