f5a1d3be679b4dab2337c5c43aa86b5af3818460
Vladimir <htcvlado@gmail.com>
2022-06-25 00:55:26 +0200

    Code cleanup

--- modules/db/manager.py
+++ modules/db/manager.py
@@ -1,10 +1,8 @@
 from sqlalchemy.orm import sessionmaker
 from instagrapi.types import Comment, UserShort
+from typing import List
 from modules.db.db_tables import engine, CommentModel, UserModel
 # https://stackoverflow.com/a/16434931
-
-Session = sessionmaker(bind=engine)
-session = Session()
 
 # INSERTING A NEW USER
 # user = UserModel(pk="314311414519", username="John", fullname="John Doe")
@@ -25,13 +23,27 @@ session = Session()
 
 # GETTING COMMENTS OF A USER
 # print(user.comments[0].pk)
+# print(session.query(CommentModel).filter_by(pk="4109").first().user.username)
+# print(session.query(CommentModel).all())
 
 
 class Database:
     def __init__(self, session):
         self.session = session
 
-    def insert_user(self, new_user: UserShort):
+    def get_comment(self, pk, **kwargs) -> CommentModel:
+        return self.session.query(CommentModel).filter_by(pk=pk, **kwargs).first()
+
+    def get_user(self, pk, **kwargs) -> UserModel:
+        return self.session.query(UserModel).filter_by(pk=pk, **kwargs).first()
+
+    def get_all_comments(self) -> List[CommentModel]:
+        return self.session.query(CommentModel).all()
+
+    def get_all_users(self) -> List[UserModel]:
+        return self.session.query(UserModel).all()
+
+    def insert_user(self, new_user: UserShort) -> UserModel:
         user = self.session.query(UserModel).filter_by(pk=new_user.pk).first()
         if not user:
             # Create new user
@@ -45,7 +57,7 @@ class Database:
 
         return user
 
-    def insert_comment(self, new_comment: Comment):
+    def insert_comment(self, new_comment: Comment) -> CommentModel:
         comm = self.session.query(CommentModel).filter_by(pk=new_comment.pk).first()
         if not comm:
 
@@ -64,3 +76,10 @@ class Database:
             self.session.commit()
 
         return comm
+
+
+if __name__ == '__main__':
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    db = Database(session)
