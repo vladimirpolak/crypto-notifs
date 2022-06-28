209eada896d2484d2669948a23fa7c6a2920ec57
Vladimir <htcvlado@gmail.com>
2022-06-28 18:41:07 +0200

    Created get_detailed_coins function

--- modules/cg/manager.py
+++ modules/cg/manager.py
@@ -1,8 +1,10 @@
 from pycoingecko import CoinGeckoAPI
+from modules.cg.models import Price, Coin
+from typing import List
 cg = CoinGeckoAPI()
 
-btc_price = cg.get_price(ids='bitcoin', vs_currencies='usd')
-print(btc_price)
+# btc_price = cg.get_price(ids='bitcoin', vs_currencies='usd')
+# print(btc_price)
 
 
 def get_price(coin_id: str, currency: str = "usd") -> int:
@@ -10,7 +12,8 @@ def get_price(coin_id: str, currency: str = "usd") -> int:
     return int(r[coin_id][currency])
 
 # Multiple coins/currencies in one request
-# cg.get_price(ids=["bitcoin", "ethereum"], vs_currencies=["usd", "eur"])
+# coins = cg.get_price(ids=["bitcoin", "ethereum"], vs_currencies=["usd", "eur"])
+# print(coins)
 
 # Price of single coin
 # price = get_price(
@@ -18,8 +21,29 @@ def get_price(coin_id: str, currency: str = "usd") -> int:
 #     currency='usd'
 # )
 
-# coins_detailed = cg.get_coins() # Get detailed list of coins
-#
-# # coins = cg.get_coins_list() # Get basic list of coins
-# for coin in coins_detailed:
-#     print(coin["name"])
+
+def get_detailed_coins() -> List[Coin]:
+    """
+    Returns list of coins with their current market prices.
+    Will be used to initializing database as after that we will
+    only get prices for requested coins and update their prices accordingly.
+
+    :return: List of Coin classes
+    """
+    coins_detailed = cg.get_coins() # Get detailed list of coins
+
+    output = []
+    for coin in coins_detailed:
+        price_data = coin["market_data"]["current_price"]
+        price = [Price(currency=currency, value=value) for currency, value in price_data.items()]
+        coin = Coin(
+            name=coin["id"],
+            symbol=coin["symbol"],
+            price=price
+        )
+        output.append(coin)
+
+    return output
+
+
+# coins = cg.get_coins_list() # Get basic list of coins
