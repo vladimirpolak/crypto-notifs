ef7366a6b51e32bf6c410e518deadaf134ed4d39
Vladimir <htcvlado@gmail.com>
2022-06-29 15:56:31 +0200

    Added function description.

--- modules/comment_validation.py
+++ modules/comment_validation.py
@@ -1,13 +1,22 @@
 import re
 
 
-def verify_comment(comment: str):
+def verify_comment(comment: str) -> tuple:
+    """
+
+    :param comment: string
+    :return: tuple (crypto_coin_symbol,
+                    condition (</>),
+                    target_value,
+                    currency (optional, None if not specified)
+    """
     pattern = re.compile("([a-z]+?)([<>])([0-9]+)([a-z]+)?")
     match = re.match(pattern, comment)
 
     if not match:
         return None
 
+    # Extract values
     coin_symbol = match.group(1)
     condition = match.group(2)
     value = int(match.group(3))
