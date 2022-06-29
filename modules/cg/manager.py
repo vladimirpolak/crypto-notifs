711b8c501c1c42f2c728c96f646dbcf3dcfe6f46
Vladimir <htcvlado@gmail.com>
2022-06-29 18:19:13 +0200

    Created CoinGecko class for import convenience.

--- modules/cg/manager.py
+++ modules/cg/manager.py
@@ -1,15 +1,15 @@
-from pycoingecko import CoinGeckoAPI
+from pycoingecko import CoinGeckoAPI as CG
 from modules.cg.models import Price, Coin
 from typing import List
-cg = CoinGeckoAPI()
+
 
 # btc_price = cg.get_price(ids='bitcoin', vs_currencies='usd')
 # print(btc_price)
 
 
-def get_price(coin_id: str, currency: str = "usd") -> int:
-    r = cg.get_price(ids=coin_id, vs_currencies=currency)
-    return int(r[coin_id][currency])
+# def get_price(coin_id: str, currency: str = "usd") -> int:
+#     r = cg.get_price(ids=coin_id, vs_currencies=currency)
+#     return int(r[coin_id][currency])
 
 # Multiple coins/currencies in one request
 # coins = cg.get_price(ids=["bitcoin", "ethereum"], vs_currencies=["usd", "eur"])
@@ -22,28 +22,29 @@ def get_price(coin_id: str, currency: str = "usd") -> int:
 # )
 
 
-def get_detailed_coins() -> List[Coin]:
-    """
-    Returns list of coins with their current market prices.
-    Will be used to initializing database as after that we will
-    only get prices for requested coins and update their prices accordingly.
+class CoinGecko(CG):
+    def get_detailed_coins(self) -> List[Coin]:
+        """
+        Returns list of coins with their current market prices.
+        Will be used to initializing database as after that we will
+        only get prices for requested coins and update their prices accordingly.
 
-    :return: List of Coin classes
-    """
-    coins_detailed = cg.get_coins() # Get detailed list of coins
+        :return: List of Coin classes
+        """
+        coins_detailed = self.get_coins() # Get detailed list of coins
 
-    output = []
-    for coin in coins_detailed:
-        price_data = coin["market_data"]["current_price"]
-        price = [Price(currency=currency, value=value) for currency, value in price_data.items()]
-        coin = Coin(
-            name=coin["id"],
-            symbol=coin["symbol"],
-            price=price
-        )
-        output.append(coin)
+        output = []
+        for coin in coins_detailed:
+            price_data = coin["market_data"]["current_price"]
+            price = [Price(currency=currency, value=value) for currency, value in price_data.items()]
+            coin = Coin(
+                name=coin["id"],
+                symbol=coin["symbol"],
+                price=price
+            )
+            output.append(coin)
 
-    return output
+        return output
 
 
 # coins = cg.get_coins_list() # Get basic list of coins
